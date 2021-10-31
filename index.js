39b428407c9c4062bab6144875dc3d27065ce1f1
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-10-31 20:06:39 +0530

    [add] single booking delete api

--- index.js
+++ index.js
@@ -66,6 +66,15 @@ async function run() {
             res.json(result);
         })
 
+        //DELETE BOOKING API
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            console.log(result);
+            res.json(result);
+        })
+
 
 
     } finally {
